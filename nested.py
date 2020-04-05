0d77cdfc9db0fabdc9adcb3946e3b4df0f06af76
kehindeadedara <35585101+kehindeadedara@users.noreply.github.com>
2020-04-05 17:54:28 -0400

    Add files via upload

---
+++ nested.py
@@ -0,0 +1,109 @@
+import json
+def extract_element_from_json(obj, path):
+    '''
+    Extracts an element from a nested dictionary or
+    a list of nested dictionaries along a specified path.
+    If the input is a dictionary, a list is returned.
+    If the input is a list of dictionary, a list of lists is returned.
+    obj - list or dict - input dictionary or list of dictionaries
+    path - list - list of strings that form the path to the desired element
+    '''
+    def extract(obj, path, ind, arr):
+        '''
+            Extracts an element from a nested dictionary
+            along a specified path and returns a list.
+            obj - dict - input dictionary
+            path - list - list of strings that form the JSON path
+            ind - int - starting index
+            arr - list - output list
+        '''
+        key = path[ind]
+        if ind + 1 < len(path):
+            if isinstance(obj, dict):
+                if key in obj.keys():
+                    extract(obj.get(key), path, ind + 1, arr)
+                else:
+                    arr.append(None)
+            elif isinstance(obj, list):
+                if not obj:
+                    arr.append(None)
+                else:
+                    for item in obj:
+                        extract(item, path, ind, arr)
+            else:
+                arr.append(None)
+        if ind + 1 == len(path):
+            if isinstance(obj, list):
+                if not obj:
+                    arr.append(None)
+                else:
+                    for item in obj:
+                        arr.append(item.get(key, None))
+            elif isinstance(obj, dict):
+                arr.append(obj.get(key, None))
+            else:
+                arr.append(None)
+        return arr
+    if isinstance(obj, dict):
+        return extract(obj, path, 0, [])
+    elif isinstance(obj, list):
+        outer_arr = []
+        for item in obj:
+            outer_arr.append(extract(item, path, 0, []))
+        return outer_arr
+
+with open("111.json",'r') as data:
+    data = json.load(data)
+print(data)
+#
+output = extract_element_from_json(data,["dates","sessions"])
+out2 = extract_element_from_json(output,["activities","time"]),
+out3 = extract_element_from_json(out2,["Content description",""])
+
+def pretty_search(dict_or_list, key_to_search, search_for_first_only=False):
+    """
+    Give it a dict or a list of dicts and a dict key (to get values of),
+    it will search through it and all containing dicts and arrays
+    for all values of dict key you gave, and will return you set of them
+    unless you wont specify search_for_first_only=True
+
+    :param dict_or_list:
+    :param key_to_search:
+    :param search_for_first_only:
+    :return:
+    """
+    search_result = set()
+    if isinstance(dict_or_list, dict):
+        for key in dict_or_list:
+            key_value = dict_or_list[key]
+            if key == key_to_search:
+                if search_for_first_only:
+                    return key_value
+                else:
+                    search_result.add(key_value)
+            if isinstance(key_value, dict) or isinstance(key_value, list) or isinstance(key_value, set):
+                _search_result = pretty_search(key_value, key_to_search, search_for_first_only)
+                if _search_result and search_for_first_only:
+                    return _search_result
+                elif _search_result:
+                    for result in _search_result:
+                        search_result.add(result)
+    elif isinstance(dict_or_list, list) or isinstance(dict_or_list, set):
+        for element in dict_or_list:
+            if isinstance(element, list) or isinstance(element, set) or isinstance(element, dict):
+                _search_result = pretty_search(element, key_to_search, search_result)
+                if _search_result and search_for_first_only:
+                    return _search_result
+                elif _search_result:
+                    for result in _search_result:
+                        search_result.add(result)
+    return search_result if search_result else None
+k = "duration"
+t = "time"
+g = pretty_search(output,k,False)
+m = pretty_search(output,t,False)
+print("\n")
+print(" Total Duration is:")
+print(g)
+print("Total user's time is:")
+print(m)
